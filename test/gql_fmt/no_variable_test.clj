e6c46cd0f60e22d6f9d0bc2243c64e7c45cfd9f9
snasphysicist <ih8coldplay@hotmail.com>
2024-03-05 19:31:35 +0000

    feat: test with two field names, passing

--- test/gql_fmt/no_variable_test.clj
+++ test/gql_fmt/no_variable_test.clj
@@ -12,3 +12,14 @@
         reformatted (format/reformat
                      q)]
     (test/is (= expect reformatted))))
+
+(test/deftest query-with-two-unnested-field-names-no-parameters-test
+  (let [q "query{user account}"
+        expect (str
+                "query {" "\n"
+                "  user" "\n"
+                "  account" "\n"
+                "}")
+        reformatted (format/reformat
+                     q)]
+    (test/is (= expect reformatted))))
