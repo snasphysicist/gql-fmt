5546dbfffb966bac1cff880090ffb81feb1e6c03
snasphysicist <ih8coldplay@hotmail.com>
2024-03-14 08:12:47 +0000

    fix: description and licence updated in project file

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject gql-fmt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A GraphQL query formatter"
+  :license {:name "MIT"
+            :url "https://mit-license.org"}
   :dependencies [[alumbra/parser "0.1.7"]
                  [com.taoensso/timbre "6.3.1"]
                  [org.clojure/clojure "1.11.1"]]
+  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :main ^:skip-aot gql-fmt.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
