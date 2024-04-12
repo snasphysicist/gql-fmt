6dd0e0d5ec5b78b7d7fb1d95a3814c4398145dd2
snasphysicist <ih8coldplay@hotmail.com>
2024-04-12 11:14:00 +0100

    fix: don't exclude main from aot

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[alumbra/parser "0.1.7"]
                  [com.taoensso/timbre "6.3.1"]
                  [org.clojure/clojure "1.11.1"]]
-  :main ^:aot gql-fmt.core
+  :main gql-fmt.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
