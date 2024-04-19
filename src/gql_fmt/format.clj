68c3122263211886a8b29be99084d469331ad06b
snasphysicist <ih8coldplay@hotmail.com>
2024-04-19 14:07:52 +0100

    fix: allow log level to be set by environment variable, default to error

--- src/gql_fmt/format.clj
+++ src/gql_fmt/format.clj
@@ -1,12 +1,32 @@
 (ns gql-fmt.format
   (:require
    [alumbra.parser :as parser]
+   [clojure.string :as string]
    [gql-fmt.configuration :as configuration]
    [gql-fmt.intermediate.operation :as intermediate.operation]
    [gql-fmt.intermediate.print :as print]
    [taoensso.timbre :as logging]))
 
 (set! *warn-on-reflection* true)
+
+(defn ^:private set-log-level!
+  "Sets the log level from the environment,
+   if given, else defaults to :error"
+  []
+  (let [from-environment (System/getenv
+                          "GQL_FMT_LOG_LEVEL")
+        level (if (string/blank? from-environment)
+                "error"
+                from-environment)
+        as-keyword (-> level
+                       string/lower-case
+                       keyword)]
+    (assert
+     (#{:trace :debug :info :warn :error :fatal :report}
+      as-keyword))
+    (logging/set-min-level! as-keyword)))
+
+(set-log-level!)
 
 (defn reformat
   "Reformat given string query `q`
